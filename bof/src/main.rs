5d2a0767ac2993484fc25f7d171967597039bcb5
LilianBernot <lilian.bernot@gmail.com>
2025-01-07 11:01:36 +0100

    feat(showdir): improve command by indenting display

--- bof/src/main.rs
+++ bof/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 
     match args[1].as_str() {
         "init" => init_command(),
-        "showdir" => showdir_command("./"),
+        "showdir" => showdir_command("./", 0),
         _ => {
             eprintln!("Unknown command: {}", args[1]);
             process::exit(1);
@@ -32,23 +32,47 @@ fn init_command() {
     }
 }
 
-fn showdir_command(current_path:&str) {
+/// Recursively displays the directory structure starting from a given path.
+///
+/// This function traverses the directory tree starting at the specified `current_path`,
+/// printing the directory and file names indented to reflect their depth in the tree.
+/// For subdirectories, the function calls itself recursively to continue the traversal.
+///
+/// # Arguments
+///
+/// * `current_path` - A string slice representing the path of the directory to start from.
+/// * `depth` - The current depth level, used to calculate the indentation for display purposes.
+///
+/// # Panics
+///
+/// This function will panic if it encounters an error reading the directory specified by `current_path`.
+///
+/// # Examples
+///
+/// ```
+/// showdir_command("./my_directory", 0);
+/// ```
+///
+/// This will display the structure of `./my_directory` and its subdirectories.
+fn showdir_command(current_path:&str, depth: usize) {
 
     let current_path_unwrap = fs::read_dir(current_path).unwrap();
+
+    let display_prefix = "\t".repeat(depth);
 
     for path in current_path_unwrap {
         let unwrapped_path = path.unwrap().path();
 
         let path_name = unwrapped_path.display().to_string();
 
-        println!("{}", path_name);        
+        println!("{} {}", display_prefix, path_name);        
 
         if unwrapped_path.is_dir() {
 
             if path_name == "./target/debug" {
-                println!("\t...");
+                println!("{}\t...", display_prefix);
             } else {
-                showdir_command(&path_name);
+                showdir_command(&path_name, depth+1);
             }
         }
     }
